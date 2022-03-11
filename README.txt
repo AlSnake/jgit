499bee2e44f50d3dcb1a0774c5bfacdc98d8b8e1
AlSnake <notalsnake@gmail.com>
2022-03-11 17:56:16 -0500

    init commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+JGit
+A Git Implementation in Java
+AUTHOR: AlSnake
+License: GNU General Public License v3.0
+
+#UNDERDEVELOPMENT
---
+++ src/main/java/com/github/alsnake/jgit/JGit.java
@@ -0,0 +1,13 @@
+package com.github.alsnake.jgit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JGit {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JGit.class);
+
+    public static void main(String[] args) {
+        System.out.println("MAIN");
+        LOGGER.info("LOGBACK2");
+    }
+}
